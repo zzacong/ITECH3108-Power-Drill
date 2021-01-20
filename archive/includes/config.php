793c4f21bb037a5d5aeea2542ede6b13a82bcc17
zzacong <ongzz100@gmail.com>
2021-01-20 23:35:29 +1100

    html overhaul

--- archive/includes/config.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-$DB_DSN = 'mysql:dbname=itech3108_30360914_a2;host=localhost;charset=utf8mb4';
-$DB_USER = 'grapevine';
-$DB_PASSWD = 'password';
-
-?>
--- archive/includes/sql.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-$sql = "
-  SELECT * FROM PowerDrilPost
-";
-
-?>
